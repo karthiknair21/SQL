fbd4751bf6e3cc2417698483370bd3bc60d0c9c3
Karthik Nair <37780805+karthiknair21@users.noreply.github.com>
2019-04-08 23:28:17 -0400

    Update Copy and Check-Procedure(bulk insert).sql

--- Copy and Check-Procedure(bulk insert).sql
+++ Copy and Check-Procedure(bulk insert).sql
@@ -1,18 +1,18 @@
-USE [FP8_Src]
+USE [db_Src]
 GO
 /****** Object:  StoredProcedure [dbo].[CopyNCheck_FP8]    Script Date: 3/8/2017 3:51:49 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-ALTER procedure [dbo].[CopyNCheck_FP8] @DestTable as nvarchar(max)
+ALTER procedure [dbo].[CopyNCheck_db] @DestTable as nvarchar(max)
 as
 begin
 
 Declare @sql nvarchar(max)
 declare @SourceTbl nvarchar(max)
 set @SourceTbl=parsename(@DestTable,1)+'_src'
-set @sql='Use FP8_Src'
+set @sql='Use db_Src'
 --creating table structure
 
 IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME=@SourceTbl)
@@ -34,7 +34,7 @@ print @sql
 exec(@sql)
 
 
-set @sql='Bulk insert ['+parsename(@DestTable,1)+'_Src] FROM ''F:\FP8\'+parsename(@DestTable,1)+'.txt''' +' WITH(
+set @sql='Bulk insert ['+parsename(@DestTable,1)+'_Src] FROM ''F:\db\'+parsename(@DestTable,1)+'.txt''' +' WITH(
 FIELDTERMINATOR = ''$|$'',
 ROWTERMINATOR = ''\n'',
 MAXERRORS = 1000
@@ -42,9 +42,9 @@ MAXERRORS = 1000
 print @sql
 exec(@sql)
 
-set @sql='Use FP8_Src'
+set @sql='Use db_Src'
 exec(@sql)
-set @sql='CheckRowCount @SrcTbl=''[FP8_Src].[dbo].['+parsename(@DestTable,1)+'_Src]'',@DestTbl=''[FP8_Trg].[FINWARE].['+parsename(@DestTable,1)+']'''
+set @sql='CheckRowCount @SrcTbl=''[db_Src].[dbo].['+parsename(@DestTable,1)+'_Src]'',@DestTbl=''[db_Trg].[dbo].['+parsename(@DestTable,1)+']'''
 print @sql
 exec(@sql)
 end;
